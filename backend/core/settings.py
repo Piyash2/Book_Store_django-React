69cf5b3340e18ecb71904505dff90021fa6c1c83
Piyash2 <piyash2429@gmail.com>
2025-08-08 21:08:48 +0600

    added allowed origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["book-store-django-react.onrender.com/", "127.0.0.1", "localhost"]
 
 
 # Application definition
